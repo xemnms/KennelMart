8b014c7e566387505013ef234992a7db57e16016
Axel <axdrbagay@gmail.com>
2026-05-21 13:27:26 +0000

    Project Initialization

---
+++ kennelmart/src/main/java/com/kennel/mart/kennelmart/KennelmartApplication.java
@@ -0,0 +1,13 @@
+package com.kennel.mart.kennelmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KennelmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KennelmartApplication.class, args);
+	}
+
+}
---
+++ kennelmart/src/test/java/com/kennel/mart/kennelmart/KennelmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kennel.mart.kennelmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KennelmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
